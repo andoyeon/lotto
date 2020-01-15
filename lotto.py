ac660b5b6ab2720dd6315431ab6072def394c848
andoyeon <56617914+andoyeon@users.noreply.github.com>
2020-01-15 23:32:28 +0900

    .

--- lotto.py
+++ lotto.py
@@ -8,20 +8,24 @@ from collections import Counter
 # 숫자 조합
 
 if __name__ == '__main__':
+    col_names = ['1', '2', '3', '4', '5', '6', 'bonus']
     dataset = pd.read_csv('lotto.csv', thousands=',', header=None, encoding='utf-8')
-    # print(dataset.iloc[3, 26:])
-    # print(dataset.shape)
-    # print(dataset.iloc[:, -7:])
-    lotto = dataset.iloc[3:, -7:]
-    print(lotto)
-    lotto_list = []
-    for i in range(dataset.index):
-        for j in range(dataset.columns):
-            num = dataset.loc[i, j]
-            lotto_list.append(num)
+    print(dataset.shape)
+    dataset = dataset.iloc[3:, -7:]
+    print(dataset)
+    dataset_except_bonus = dataset.iloc[:, :-1]
+    print(dataset_except_bonus)
 
-    # lotto_list = [dataset.loc[i, j] for i in dataset.index for j in dataset.columns]
-    print(lotto_list)
-    # count = Counter(lotto)
-    # print(count)
+    # 단순 분석
+    # 보너스 포함
+    lotto_list = [dataset.loc[i, j] for i in dataset.index for j in dataset.columns]
+    lotto_list = map(int, lotto_list)
+    count = Counter(lotto_list)
+    print(count)
+
+    # 보너스 제외
+    lotto_except_bonus_list = [dataset.loc[i, j] for i in dataset.index for j in dataset.columns]
+    lotto_except_bonus_list = map(int, lotto_except_bonus_list)
+    count = Counter(lotto_except_bonus_list)
+    print(count)
 
